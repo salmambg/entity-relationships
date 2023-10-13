d2a5e5b3f286cfdb2cd07191db0294dcbe4e1c51
salma <gsalmahossain@gmail.com>
2023-10-13 17:32:56 +0200

    bidirectional relationship implementation between student and passport

--- src/main/java/org/example/entity/Passport.java
+++ src/main/java/org/example/entity/Passport.java
@@ -5,7 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -14,6 +17,9 @@ public class Passport {
     @Id
     private long id;
     private String number;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "passport")
+    private Student student;
 
     @Override
     public String toString() {
